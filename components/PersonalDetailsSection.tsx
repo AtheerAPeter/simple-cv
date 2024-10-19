03cd5fa448daa34603d3a873f0c96219bf2c4271
AtheerAPeter <atheer07713991785@gmail.com>
2024-10-19 12:57:19 +0200

    fix: imports

--- components/PersonalDetailsSection.tsx
+++ components/PersonalDetailsSection.tsx
@@ -1,6 +1,5 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useUploadImage } from "@/hooks/useUploadImage";
 import { Trash2Icon } from "lucide-react";
 import { useTranslations } from "next-intl";
 import { Button } from "./ui/button";
